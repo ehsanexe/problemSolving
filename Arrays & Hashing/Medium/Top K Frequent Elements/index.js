1cee9081892985e107fcf2b689de169b16d3f1f9
Ehsan <ehsanexe01@gmail.com>
2024-08-13 14:47:23 +0500

    update 'Arrays & Hashing/Medium/Top K Frequent Elements/index.js'

--- Arrays & Hashing/Medium/Top K Frequent Elements/index.js
+++ Arrays & Hashing/Medium/Top K Frequent Elements/index.js
@@ -17,6 +17,26 @@ Output: [1]
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {
-    
+var topKFrequent = function (nums, k) {
+  const map = {};
+
+  for (const n of nums) {
+    map[n] = (map[n] ?? 0) + 1;
+  }
+
+  const freq = [];
+  for (const key in map) {
+    freq.push({ key, val: map[key] });
+  }
+
+  return freq
+    .sort((a, b) => b.val - a.val)
+    .slice(0, k)
+    .map((f) => parseInt(f.key));
 };
+
+// todo- this solution is O(NLogN), there is a batter to solve this using bucket sort
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2)); // [1,2]
+console.log(topKFrequent([1], 1)); // [1]
+console.log(topKFrequent([4, 1, -1, 2, -1, 2, 3], 2)); // [2,-1]
